1505550dd7b0df14967fe00709654bb32412002f
SDI <staugur@vip.qq.com>
2015-07-11 01:12:29 +0800

    完善修葺web

--- spmc/post.php
+++ spmc/post.php
@@ -1,19 +1,26 @@
+<?php   
+$url = "https://ci.saintic.com/"; ?> 
 <html>
-<body>
-
+ <head>
+  <meta http-equiv="refresh" content="2; url=<?php echo $url; ?>">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sdp.SPMC | Simple Docker PaaS</title>
+ </head>
+ <body>
 <?php
+//disable error reporting.
+error_reporting(0);
 $user = $_POST['user'];
 $time = $_POST['time'];
 $service = $_POST['service'];
 $file = $_POST['file'];
 $email = $_POST['email'];
-system("echo YouPassword | sudo -u root -S /bin/sh /data/sdp/start.sh $user $time $service $file $email 2>&1",$status);
+system("echo Sdp.SaintIC.com@2016 | sudo -u root -S /bin/sh -x /data/sdp/start.sh $user $time $service $file $email",$status);
 if($status == 'true') {
   echo "<script>alert('Build Seccuss!');</script>";
 } else {
   echo "<script>alert('Build Wrong!');</script>";
 }
 ?>
-
-</body>
+ </body>
 </html>
