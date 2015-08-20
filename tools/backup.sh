1968e3c5aafedede1c95d3880f98c46f51fe1064
taochengwei <staugur@vip.qq.com>
2015-08-20 13:20:52 +0800

    备份脚本

--- tools/backup.sh
+++ tools/backup.sh
@@ -2,6 +2,23 @@
 #Backup user data
 ScriptDIR=$(cd `dirname $0`; pwd)
 source ${ScriptDIR}/functions.sh
-export LANG=zh_CN.UTF-8
 
+BakDir="/data/backup/"
+DateTime=$(date +"%Y%m%d")
+LogFile="${BakDir}/backup.log"
+#Log Format:
+#PreciseTime user service:create_time~exp_time backup_file
 
+[ -d ${BakDir} ] || mkdir -p $BakDir
+for user in Users ; do
+  userbackupfile="${BakDir}/${DateTime}.tar.gz"
+  userjson=${SdpDataHOME}/${user}/user.json
+  UHome=$(jq '.home' ${userjson})
+  UService=$(jq '.service' $userjson)
+  UCreateTime=$(jq '.CreateTime' $userjson|awk -F \" '{print $2}')
+  UExpirationTime=$(jq '.ExpirationTime' $userjson|awk -F \" '{print $2}')
+  mkdir -p ${BakDir}/${user}
+  tar zcf ${userbackupfile} $UHome
+  echo "
+${PreciseTime} ${user} ${UService}:${UCreateTime}~${UExpirationTime} ${userbackupfile}" >> ${LogFile}
+done
