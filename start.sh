701c240bbfff6d51303a7587d25b0806ac9895bb
taochengwei <staugur@vip.qq.com>
2015-08-28 03:50:17 +0800

    all check

--- start.sh
+++ start.sh
@@ -6,16 +6,41 @@ source ${SDP_HOME}/global.func
 
 #Logs
 [ -d $LogDir ] || mkdir -p $LogDir
+[ -d $INIT_HOME] || mkdir -p $INIT_HOME
 
 #判断入参及入参要求是否符合。
 if [ "$#" = 5 ]; then
+  #判断用户是否存在
+  if [ -d ${INIT_HOME}/$1 ]; then
+    echo -e "\033[31mThe user already exists\033[0m" 2>&1
+    echo "${PreciseTime} $1 $5 ErrAction:\"The user already exists\"" >> $Errlog
+	exit 1
+  fi
+  #判断时间格式
   if [[ "$2" =~ ^[0-9]+$ ]]; then
     :
   else
     echo "第二个参数要求为正整数，单位为月！"
-    echo "${PreciseTime} ${init_user} ${user_email} ErrAction:\"使用时间参数错误\" " >> $Errlog
+    echo "${PreciseTime} $1 $5 ErrAction:\"使用时间参数错误\" " >> $Errlog
     exit 1;
   fi
+  #判断服务类型
+  if echo "${services[@]}" | grep -w $3 &> /dev/null ;then
+    :
+  else
+    echo -e "\033[31m不支持的服务类型\033[0m" 2>&1
+    echo "${PreciseTime} $1 $5 ErrAction:\"不支持的服务类型\"" >> $Errlog
+	exit 1
+  fi
+  #判断文件代码类型
+  if [ $4 = "svn" ] || [ $4 = "ftp" ] || [ $4 = "-" ] || [ $4 = "null" ];then
+    :
+  else
+    echo -e "\033[31m不支持的代码类型\033[0m" 2>&1
+    echo "${PreciseTime} $1 $5 ErrAction:\"不支持的代码类型\"" >> $Errlog
+	exit 1
+  fi
+  #判断邮箱格式
   if [[ `echo $5 | sed -r '/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/!d'` == "" ]]; then
     echo "邮箱格式不正确！"
     echo "${PreciseTime} ${init_user} ${user_email} ErrAction:\"邮箱格式不正确\" " >> $Errlog
@@ -24,8 +49,7 @@ if [ "$#" = 5 ]; then
 elif [ "$#" != "5" ]; then
   echo -e "\033[31mUsage: $0 user use_time service_type file_type email\033[0m"
   cat << HELP
-此脚本需要五个入参，分别是user(用户) use_time(使用时间，单位月) service_type(服务) file_type(文件类型) email(用户邮箱)。
-说明:
+此脚本需要五个入参:
 1).用户名若发生冲突，同个用户需要多个服务当前版本必须多次以不同用户名执行;
 2).使用时间不限，至少1个月;
 3).服务类型：nginx、httpd、tomcat、mysql、mongodb、redis、memcached;
@@ -47,16 +71,6 @@ export init_user_home=${INIT_HOME}/$init_user           #directory
 export init_user_home_info=${init_user_home}/info       #file
 export init_user_home_root=${init_user_home}/root       #directory
 
-#创建Sdp数据根目录
-if [ -d $INIT_HOME ]; then
-  if [ -d $init_user_home ]; then
-    echo -e "\033[31mThe user already exists\033[0m" 2>&1
-    echo "${PreciseTime} ${init_user} ${user_email} ErrAction:\"The user already exists\"" >> $Errlog
-	exit 1
-  fi
-else
-  mkdir -p $INIT_HOME
-fi
 
 #创建Sdp用户数据文件，JSON格式，方便tools工具后续工作。
 [ -f $Sdpuc ] || cat > $Sdpuc <<EOF
