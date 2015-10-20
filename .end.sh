b15779400691f43c76dfe3cb78e5fb3698633285
taochengwei <staugur@vip.qq.com>
2015-10-20 23:47:32 +0800

    发邮件方式，默认是shell，如果出现不能接收邮件现象，请选择python方式

--- .end.sh
+++ .end.sh
@@ -137,7 +137,7 @@ if [ -d $init_user_home ]; then
     grep $init_user_dns	$dnmap_file &> /dev/null || DoubleError
   fi
   echo "Ending,Succeed!!!"
-  pythonemail
+  shellemail
   echo "{\"${PreciseTime}\":{\"user\": \"${init_user}\",\"id\": \"${user_id}\",\"email\": \"${user_email}\",\"service\": \"${init_service_type}\",\"container\": \"${container_id}\",\"time\": \"${CreateTime}~${ExpirationTime}\",\"code\": \"${init_file_type}\"}}" >> $Suclog
   echo >> $Suclog
 else
