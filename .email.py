25fc1fb13bf632b5732a1cf986edc013cecbb6d0
陶成伟 <staugur@vip.qq.com>
2015-11-03 15:52:47 +0800

    修改某些邮箱（腾讯企业邮）无法接收邮件的问题

--- .email.py
+++ .email.py
@@ -9,9 +9,9 @@ def _format_addr(s):
   name, addr = parseaddr(s)
   return formataddr((Header(name, 'utf-8').encode(), addr))
 
-from_addr='SdpCenter@saintic.com'
-#password
-smtp_server='127.0.0.1'
+from_addr='sdp@saintic.net'
+password="SaintAugur910323"
+smtp_server='smtp.saintic.net'
 
 if len(sys.argv) == 4:
   #user_name user_email and content_file
@@ -19,18 +19,18 @@ if len(sys.argv) == 4:
   to_addr=sys.argv[2]
   content_file=sys.argv[3]
 else:
-  print "Into the reference error, three parameters are required."
+  print "Into the reference error, three parameters are required:user email info_file."
   sys.exit(1)
 
 with open(content_file) as f:
   content=f.read()
+
 msg = MIMEText(content, 'html', 'utf-8')
-msg['From'] = _format_addr('SdpCenter <%s>' % from_addr)
+msg['From'] = _format_addr('SdpTeam <%s>' % from_addr)
 msg['To'] = _format_addr('%s <%s>' % (user,to_addr))
-msg['Subject'] = Header('来自Sdp系统的消息', 'utf-8').encode()
+msg['Subject'] = Header('来自SdpTeam的系统消息', 'utf-8').encode()
 server=smtplib.SMTP(smtp_server, 25)
 #server.set_debuglevel(1)
-#server.login(from_addr, password)
+server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
-
