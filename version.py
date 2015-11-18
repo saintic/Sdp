6014179db63735c36f6f0e2b00efe1794ecf60a5
taochengwei <staugur@vip.qq.com>
2015-11-18 23:24:35 +0800

    Sdp.py copy ci

---
+++ version.py
@@ -0,0 +1,11 @@
+#coding:utf8
+version = 1.1
+"""
+V1.1:
+    本次迭代更新的功能：1、全Py实现的框架基本功能；2、代码管理方式FTP；3、IP+PORT省略静态IP设置；4、Nginx代理实现；
+V1.1.x:
+    在补充版本中添加pre_check类，针对user预检测，读取redis等以防止存在已注册用户。
+V1.2:
+    主要是PythonWeb用户注册管理服务系统和SVN+FTP集成代码管理。
+"""
+
