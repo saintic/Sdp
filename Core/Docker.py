e14a23cd7fa72e58d0ce1c2bbc15a3f0c61308b7
taochengwei <staugur@vip.qq.com>
2015-12-09 18:54:24 +0800

    docker images pull push

--- Core/Docker.py
+++ Core/Docker.py
@@ -6,24 +6,43 @@ __doc__ = 'docker functions, for images, for containers'
 try:
     import docker
     import Config
+    import json
 except ImportError as errmsg:
     raise ImportError('import failed, %s' % errmsg)
 
 class Docker:
     def __init__(self):
-        self.connect = docker.Client(base_url='unix://var/run/docker.sock', version='auto')
-        #self.connect = docker.Client(base_url='tcp://127.0.0.1:2375')
+        self.connect = docker.Client(base_url='unix://var/run/docker.sock')
+
+    def images(self, image=None):
+        return self.connect.images(image)
+
+    def pull(self, image, repo=Config.DOCKER_REGISTRY, tag=None):
+        if self.connect.images(name=image):
+            return True
+        else:
+            for __line in self.connect.pull(image, repository=repo, tag=tag, stream=True):  #a generator
+                print json.dumps(json.loads(__line), indent=4)
+                return True
+
+    def push(self, image):
+        if self.connect.images(name=image):
+            if Config.DOCKER_PUSH == 'On' or Config.DOCKER_PUSH == 'on':
+                for line in self.connect.push(image, stream=True):
+                    print json.dumps(json.loads(line))
+        else:
+            raise ValueError('%s, no such image.' % image)
 
     def Create(self, **kw):
-        """The operation of the docker module is encapsulated."""
         if not isinstance(kw, (dict)):
             raise TypeError('Bad Type, ask a dict.')
+
         image=kw['image']
         name=kw.get('name', None)
-        container_port=kw.get('port', None)#container open port,int,attach cports
-        host_ip_port=kw.get('bind', None)#should be tuple,(host_ip,host_port),all is {container_port, (host_ip, host_port)}
-        volume=kw.get('volume', None) #host_dir
-        volumes=[]
+        container_port=kw.get('port', None)   #container open port,int,attach cports.
+        host_ip_port=kw.get('bind', None)     #should be tuple,(host_ip,host_port),all is {container_port, (host_ip, host_port)}.
+        volume=kw.get('volume', None)         #host_dir, default binding /data/wwwroot in container.
+
         cports=[]
         if container_port:
             cports.append(container_port)
@@ -31,30 +50,27 @@ class Docker:
         else:
             cports=None
             port_bindings=None
+
+        volumes=[]
         if volume:
-            cfs=['%s:/data/wwwroot:rw' % volume] # ask list ['container_dir:host_dir:mode(rw,ro)']
-            volumes.append(volume)     #Only this, more access=>https://github.com/docker/docker-py/issues/849
+            cfs=['%s:/data/wwwroot:rw' % volume]  #ask list ['container_dir:host_dir:mode(rw,ro)']
+            volumes.append(volume)                #Only this, more access=>https://github.com/docker/docker-py/issues/849
         else:
             cfs=None
+
         cid=self.connect.create_container(image=image, name=name, stdin_open=True, tty=True, ports=cports, volumes=None, host_config=self.connect.create_host_config(restart_policy={"MaximumRetryCount": 0, "Name": "always"}, binds=cfs, port_bindings=port_bindings), mem_limit=None, memswap_limit=None, cpu_shares=None)['Id'][:12]
-        #print '\033[0;32;40mSuccess to create container, id => %s\033[0m' % cid
+        print 'Success to create container, id => %s' % cid
         return cid
 
     def Start(self, cid):
         __r=self.connect.start(resource_id=cid)
         if __r == None:
-            #print '\033[0;32;40mSuccess to start container, id => %s\033[0m' % cid
-            return 0
+            print 'Success to start container, id => %s' % cid
+            return True
         else:
-            print "\033[0;31;40mStart failed, id => %s\033[0m" % cid
-            return 1
+            #print "\033[0;31;40mStart failed, id => %s\033[0m" % cid
+            raise
 
-    def Ci(self, image):
-        if not isinstance(image, (str)):
-            raise TypeError('Bad type, ask an image')
-        if Config.DOCKER_PUSH == 'On' or Config.DOCKER_PUSH == 'on':
-            self.connect.push(image)
-            return
-
-    def Build(self):
-        pass
+if __name__ == '__main__':
+    i=Docker
+    print i.images('registry.saintic.com/base')
