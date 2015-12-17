5ec3db9c09fa074af1a725e39d033611f34c0c34
staugur <staugur@saintic.com>
2015-12-18 01:10:26 +0800

    package and log modify

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,2 +1,17 @@
+__doc__ = r'''
+Bottom modules:
+    Docker
+    Redis
+    Config
+    Success
+
+Application modules:
+    Handler
+    Public
+    Error
+    Log
+'''
+
 __version__ = '1.1.4'
-__all__ = ['Handler', 'Public', 'Config', 'Error', 'Log']
+
+__all__ = ['Handler', 'Public', 'Error', 'Log', 'Config']
